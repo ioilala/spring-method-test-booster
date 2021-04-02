91366c373556ef115e2befebae49f5b8c3f3eb95
GitHub Action <action@github.com>
2021-04-02 07:36:41 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ioilala/springmethodtestbooster/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ioilala.springmethodtestbooster.services
+
+import com.github.ioilala.springmethodtestbooster.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/ioilala/springmethodtestbooster/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ioilala/springmethodtestbooster/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ioilala.springmethodtestbooster.services
 
+import com.github.ioilala.springmethodtestbooster.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
